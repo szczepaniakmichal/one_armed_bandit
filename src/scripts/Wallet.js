238f3e48e84ca2eea3d859abc86a837ba7788cc5
szczepaniakmichal <mssk87@gmail.com>
2020-03-01 12:20:26 +0100

    implement class Wallet + add new file Game.js

---
+++ src/scripts/Wallet.js
@@ -0,0 +1,36 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+
+        // get value of wallet
+        this.getWalletValue = function () {
+            return _money;
+        };
+
+        // check if the user has enough money in the account for play
+        this.checkCanPlay = (value) => {
+            if ( value <= _money ) {
+               return  true;
+            } else if ( value > _money) {
+               return  false;
+            } else {
+                throw new Error('incorrect data');
+            }
+        }
+
+        this.changeWalletValue = (value, type = "-") => {
+            if (typeof value === "number" && !isNaN(value) ) {
+                if (type === '+') {
+                    return _money += value;
+                } else if (type === '-') {
+                    return _money -= value;
+                }
+            } else {
+                console.log(typeof  value);
+                throw new Error("incorrect data")
+            }
+        }
+    }
+}
+
+const wallet = new Wallet(200);
