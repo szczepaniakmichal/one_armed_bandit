2934dd2ad5ada8372ee7fc2a1abeee15062e5d95
szczepaniakmichal <mssk87@gmail.com>
2020-03-01 21:30:25 +0100

    implement class Draw

---
+++ src/scripts/Draw.js
@@ -0,0 +1,19 @@
+class Draw {
+    constructor() {
+        this.options = ['red', 'green', 'blue'];
+        let _result = this.drawResult();
+        this.getDrawResult = () => _result;
+    }
+
+    // generowanie tablicy z losowymi kolorami
+    drawResult() {
+        const colors = [];
+        for (let i = 0; i < this.options.length; i++) {
+            const index = Math.floor(Math.random() * this.options.length);
+            colors.push(this.options[index]);
+        }
+        return colors;
+    }
+}
+
+const draw = new Draw();
--- src/scripts/Statistics.js
+++ src/scripts/Statistics.js
@@ -3,6 +3,7 @@ class Statistics {
         this.resultGame = [{win: true, bid: 4}, {win: false, bid: -6}, {win: true, bid: 12}, {win: false, bid: 3},  {win: false, bid: 7} ]
     }
 
+    // add current statistics of game
     addGameStatistic(win, bid) {
         const gameResult = {
             win,
@@ -12,6 +13,7 @@ class Statistics {
         this.resultGame.push(gameResult);
     }
 
+    // show game statistics
     showGameStatistic() {
         let howManyGames = this.resultGame.length;
         let wins = this.resultGame.filter(game => game.win).length;
